bbe09cebf7e5d64b2f756bb3b296f6375165b852
TamNhiDuong <yanxinyee@gmail.com>
2020-02-07 22:02:14 +0200

    event binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -10,6 +10,7 @@ export class ServerComponent {
     id: number = 4;
     status: string = 'offline';
     enableButton: boolean = false;
+    newServerStt: string = 'no new server';
 
     showStatus() {
         return this.status;
@@ -19,5 +20,9 @@ export class ServerComponent {
         setTimeout(() => {
             this.enableButton= true;
         },2000)
+    }
+
+    onCreateServer() {
+        return this.newServerStt = 'create new server';
     }
 }
